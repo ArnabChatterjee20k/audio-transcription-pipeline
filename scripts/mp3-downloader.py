7085499c91387deb4bfcb31892599415629d6802
arnab <arnabchatterjee.ac.2@gmail.com>
2025-11-15 22:05:14 +0530

    base setup * added raw scripts * added whisper server * added required packages

---
+++ scripts/mp3-downloader.py
@@ -0,0 +1,25 @@
+import subprocess
+import yt_dlp
+import os
+
+def download_raw(url, output="audio.mp3"):
+    ydl_opts = {
+        "format": "bestaudio",
+        "outtmpl": output,
+        "postprocessors": [],  # disable ffmpeg
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+def convert_with_docker(input_file, output_file):
+    cmd = [
+        "docker", "exec", "ffmpeg_container",
+        "ffmpeg", "-i", f"/media/{input_file}",
+        "-vn", "-acodec", "mp3", f"/media/{output_file}"
+    ]
+    subprocess.run(cmd, check=True)
+
+if __name__ == "__main__":
+    download_raw("https://www.youtube.com/watch?v=Uu2QK9Z9X5E")
+    # convert_with_docker("audio.webm", "audio.mp3")
---
+++ scripts/whisper.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+from pprint import pprint
+client = OpenAI(api_key="cant-be-empty", base_url="http://localhost:8001/v1/")
+
+# audio_file = open("audio.mp3", "rb")
+# transcript = client.audio.transcriptions.create(
+#     model="Systran/faster-whisper-small", file=audio_file
+# )
+
+# with open("audio.mp3", "rb") as audio_file:
+#     transcript = client.audio.transcriptions.create(
+#         model="Systran/faster-whisper-small",
+#         file=audio_file,
+#         response_format="verbose_json",       # ⬅️ needed for timestamps
+#         timestamp_granularities=["segment"]   # ⬅️ enables segment timestamps
+#     )
+
+# pprint(transcript)
+
+# print("\n--- English Translation with Timestamps ---\n")
+# for seg in transcript.segments:
+#     print(f"[{seg.start:.2f}s -> {seg.end:.2f}s] {seg.text}")
+
+with open("audio.mp3", "rb") as audio_file:
+    transcript = client.audio.translations.create(
+        model="Systran/faster-whisper-small",
+        file=audio_file,
+        response_format="srt",
+    )
+
+print(transcript)
